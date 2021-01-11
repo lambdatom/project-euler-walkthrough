378aa0f2b8d1393cb151c09848114ca43518616f
lambdatom <>
2021-01-11 17:07:15 -0500

    p0005

---
+++ projecteuler/solutions/p0005.py
@@ -0,0 +1,22 @@
+# Just test multiples of 20 mod each
+# Ignore skipping redundant factors such as 2,5,10
+def naive():
+    ni = 20
+    n = ni
+    while True:
+        for i in range(1, ni+1):
+            if n % i != 0:
+                break
+            elif i < ni:
+                continue
+            else:
+                return n
+        n = n + ni
+
+
+def solve():
+    return naive()
+
+
+if __name__ == "__main__":
+    print(naive())
---
+++ walkthrough/problems/Problem 5.md
@@ -0,0 +1,12 @@
+# Problem 5
+#### Problem
+![[p0005#]]
+#### Concepts
+#### Discussion
+
+#### Solution
+#### Answer
+<details><summary>Spoiler warning</summary>$ANSWER</details>
+
+
+#### Tags
